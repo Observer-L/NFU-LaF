4b3f4352c5f0812347273efcf67807b4dd87e176
Observer-L <observer_l@163.com>
2019-02-09 15:45:52 +0800

    add card component and fake data

---
+++ src/markers.js
@@ -0,0 +1,64 @@
+module.exports.map = {
+  map: [
+    {
+      type: 'LOST',
+      data: [
+        {
+          id: 0,
+          itemType: 'U盘',
+          itemDetail: {
+            name: '8GB金士顿U盘一块',
+            color: '红色',
+            time: '2019-02-06',
+            place: '图书馆',
+            desc: '三天前晚上在图书馆五楼丢失',
+            pics: [
+              'http://smzx.w200.mc-test.com/Uploadfile/proImg/month_20157/20157317162048012.jpg',
+              'https://img.pconline.com.cn/images/product/4257/425724/upan_kingston_DT101G2_8G_1.jpg'
+            ]
+          },
+          latitude: 23.634322,
+          longitude: 113.681223,
+
+        },
+        {
+          id: 1,
+          itemType: '耳机',
+          itemDetail: {
+            name: 'airpods',
+            color: '白色',
+            time: '2019-01-28',
+            place: '中饭',
+            desc: '',
+            pics: [
+              'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1040837481,2803247480&fm=26&gp=0.jpg'
+            ]
+          },
+          latitude: 23.634222,
+          longitude: 113.676583
+        }
+      ]
+    },
+    {
+      type: 'FOUND',
+      data: [
+        {
+          id: 2,
+          itemType: '校园卡',
+          itemDetail: {
+            name: '商学院张倩的校园卡',
+            color: '白色',
+            time: '2019-02-08',
+            place: '西饭',
+            desc: '中午在西饭一楼炒饭处找到',
+            pics: [
+              'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1533730726,3647033916&fm=26&gp=0.jpg'
+            ]
+          },
+          latitude: 23.632219,
+          longitude: 113.675001
+        }
+      ]
+    }
+  ]
+}
