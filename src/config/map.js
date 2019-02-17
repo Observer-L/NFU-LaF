e6114c8e109881cbda0ba08172fdfe3f067e2470
Observer-L <observer_l@163.com>
2019-02-17 20:38:58 +0800

    optimize code...

---
+++ src/config/map.js
@@ -0,0 +1,35 @@
+export const circlesStyle = {
+  radius: 10,
+  strokeWidth: 1,
+  lostColor: '#d81e06',
+  foundColor: '#1296db'
+}
+
+export const markersStyle = {
+  width: 36,
+  height: 36,
+  label: {
+    fontSize: 12,
+    color: '#000000',
+    bgColor: '#ffffff',
+    padding: 4,
+    borderRadius: 4,
+    anchorX: -16,
+    anchorY: -60
+  }
+}
+
+export const areas = {
+  '西区': ['西饭', '商业街', '西操', '西体', '其他'],
+  '中区': ['中饭', '音乐楼', '图书馆', '大礼堂', '铜像', '其他'],
+  '东区': ['东饭', '东操', '体育馆', '其他'],
+  '后门': ['其他']
+}
+
+export const itemType = {
+  '卡片/证件': ['校园卡', '身份证', '银行卡', '羊城通', '其他'],
+  '数码产品': ['U盘', '耳机', '手机', '鼠标', '其他'],
+  '学习用品': ['书本', '笔', '体育馆', '其他'],
+  '生活用品': ['水杯', '帽子', '其他'],
+  '其他': ['其他']
+}
