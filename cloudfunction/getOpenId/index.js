46928f022fbce20771a8a51d431b7e070711dc81
Observer-L <observer_l@163.com>
2019-02-20 20:31:09 +0800

    add close POI logic

--- cloudfunction/getOpenId/index.js
+++ cloudfunction/getOpenId/index.js
@@ -4,14 +4,13 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  console.log(wxContext)
+exports.main = (event, context) => {
+  // 这里获取到的 openId、 appId 和 unionId 是可信的，注意 unionId 仅在满足 unionId 获取条件时返回
+  const {OPENID, APPID, UNIONID} = cloud.getWXContext()
 
   return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    OPENID,
+    APPID,
+    UNIONID,
   }
 }
