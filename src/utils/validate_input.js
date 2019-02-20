408a76bc9fa300ffc95a07ef4bed6a5d4e2de1f5
Observer-L <observer_l@163.com>
2019-02-20 16:44:31 +0800

    use cloud function

--- src/utils/validate_input.js
+++
@@ -1,5 +0,0 @@
-export function validateEmail(email) {
-  const reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/
-  let isValid = reg.test(email)
-  return isValid
-}
